8b788eb66c8d2ea4cac0e7e4f41a6d714bf994e0
AntonyNishio <110011339+AntonyNishio@users.noreply.github.com>
2022-10-11 11:49:51 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,6 +2,44 @@ from typing import List
 
 def merge_sort(data) -> None:
   # Write code here
+  if len(data) > 1:
+ 
+        mid = len(data)//2
+ 
+        L = data[:mid]
+ 
+        R = data[mid:]
+ 
+        mergeSort(L)
+ 
+        mergeSort(R)
+ 
+        i = j = k = 0
+        while i < len(L) and j < len(R):
+            if L[i] <= R[j]:
+                data[k] = L[i]
+                i += 1
+            else:
+                data[k] = R[j]
+                j += 1
+            k += 1
+ 
+        while i < len(L):
+            data[k] = L[i]
+            i += 1
+            k += 1
+ 
+        while j < len(R):
+            data[k] = R[j]
+            j += 1
+            k += 1
+ 
+ 
+ 
+def printList(data):
+    for i in range(len(data)):
+        print(data[i], end=" ")
+    print()
 
 
 # Do not change the following code
