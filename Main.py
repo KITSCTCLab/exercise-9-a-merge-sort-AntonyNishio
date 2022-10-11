d526b3a9c70134aba2238ebf9b8124ccd155e633
AntonyNishio <110011339+AntonyNishio@users.noreply.github.com>
2022-10-11 12:03:08 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,45 +1,32 @@
 from typing import List
 
 def merge_sort(data) -> None:
-  # Write code here
-  if len(data) > 1:
- 
+  if len(data)>1:
         mid = len(data)//2
- 
-        L = data[:mid]
- 
-        R = data[mid:]
- 
-        merge_Sort(L)
- 
-        merge_Sort(R)
- 
-        i = j = k = 0
-        while i < len(L) and j < len(R):
-            if L[i] <= R[j]:
-                data[k] = L[i]
-                i += 1
+        lefthalf = data[:mid]
+        righthalf = data[mid:]
+
+        merge_sort(lefthalf)
+        merge_sort(righthalf)
+        i=j=k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                data[k]=lefthalf[i]
+                i=i+1
             else:
-                data[k] = R[j]
-                j += 1
-            k += 1
- 
-        while i < len(L):
-            data[k] = L[i]
-            i += 1
-            k += 1
- 
-        while j < len(R):
-            data[k] = R[j]
-            j += 1
-            k += 1
- 
- 
- 
-def printList(data):
-    for i in range(len(data)):
-        print(data[i], end=", ")
-    print()
+                data[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            data[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            data[k]=righthalf[j]
+            j=j+1
+            k=k+1
 
 
 # Do not change the following code
